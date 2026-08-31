fd8859af7c31bc13aa77eb9e95d9598271174324
anudari4k <ganbold@kth.se>
2026-08-31 16:58:17 +0200

    Update labb2.py
    uppdaterade version

--- labb2.py
+++ labb2.py
@@ -1,24 +1,62 @@
-from array import array
-färger = ["Blå", "Röd", "Lila"]
-print(färger)
-lägga_till = input("Vilken färg vill du lägga till i listan: ")
-färger.append(lägga_till)
-print(färger)
+from arrayQFile import ArrayQ
+#from array import array
+# colors = ["Blue", "Red", "Purple"]
+# print(colors)
+# add_color = input("Which color do you want to add to the list: ")
+# colors.append(add_color)
+# print(colors)
+# new_color = input("Choose another color: ")
+# position = int(input("Where do you want to insert the color? Enter the position number: "))
+# colors.insert(position, new_color)
+# print(colors)
+# remove_color = input("Which color do you want to remove from this list? : ")
+# for i in colors:
+#     if remove_color == i:
+#         colors.remove(remove_color)
+# print(colors)
+# remove_position = int(input("Enter the position number of the color you want to remove: "))
+# colors.pop(remove_position)
+# print(colors)
+# numbers = array("i", [1, 2, 3, 4, 5])  # How to create an array
 
-ny_färg = input("Välj en annan färg: ")
-plats = int(input("Var vill du sätta in färgen? ange plats nummer: "))
-färger.insert(plats, ny_färg)
-print(färger)
+# class ArrayQ:
+#     def __init__(self):
+#         self.__array = []
 
-bort = input("Från denna listan, vilken färg vill du ta bort? : ")
+#     def enqueue(self,item):
+#         self.__array.append(item)
 
-for i in färger:
-    if bort == i:
-        färger.remove(bort)
+    
+#     def dequeue(self):
+#         item = self.__array.pop(0)
+#         return item
+    
+#     __array = array
 
-print(färger)
 
-position_bort = int(input("Ge plats nummer till färg som du vill ta bort: "))
-färger.pop(position_bort)
-print(färger)
-array("i") = [1,2,3,4,5] #hur man skapar array
+#     def __str__(self):
+#         return str(self.__array[0])
+
+
+# q = ArrayQ()
+# q.enqueue(1)
+# q.enqueue(2)
+# x = q.dequeue()
+# y = q.dequeue()
+# if (x == 1 and y == 2):
+#     print("OK")
+# else:
+#     print("FAILED")
+def main():
+    nytt_q = ArrayQ()
+    kort = input("Ge 5 olika kort nummer:")
+    kortlista = kort.split(",")
+    for kort in range(len(kortlista)):
+        nytt_q.enqueue(int(kortlista[kort]))
+
+    for i in range(len(kortlista)): 
+        temp = nytt_q.dequeue()
+        nytt_q.enqueue(temp)
+        print(nytt_q) 
+        t = nytt_q.dequeue()
+main()
